24bf4fc513b70e4b30172b8f2342cb3894d3b568
Nihaarika <124379920+Nihaarika13@users.noreply.github.com>
2024-09-30 21:07:19 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install Tesseract
-apt-get update
-apt-get install -y tesseract-ocr
-apt-get install -y tesseract-ocr-hin  # For Hindi language support
-apt-get install -y tesseract-ocr-eng

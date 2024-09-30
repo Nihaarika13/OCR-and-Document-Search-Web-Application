0eeb8a4e0df264200feb92e695b63b03bd28f0e3
Nihaarika <124379920+Nihaarika13@users.noreply.github.com>
2024-09-30 20:50:39 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Tesseract
+apt-get update
+apt-get install -y tesseract-ocr
+apt-get install -y tesseract-ocr-hin  # For Hindi language support
+apt-get install -y tesseract-ocr-eng
